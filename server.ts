7817521496aa436fd04dc60e0a0d686a84888d0a
taehunlim <dsa1520@naver.com>
2022-08-19 17:06:53 +0900

    Feat: prettier

--- server.ts
+++ server.ts
@@ -1,38 +1,41 @@
-
 require('dotenv').config();
 
-import * as express from "express"
+import * as express from 'express';
+
 const app = express();
 
-const morgan = require ('morgan');
+const morgan = require('morgan');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const errorHandler = require('./_middleware/error-handler');
 
 // DB Connection
-require('./_middleware/db');
+// require('./_middleware/db');
 
 app.use(cookieParser());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-if(process.env.NODE_ENV === 'development') {
-	app.use(cors({
-		origin: process.env.CLIENT_URL_TEST
-	}))
-	app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+   app.use(
+      cors({
+         origin: process.env.CLIENT_URL_TEST,
+      }),
+   );
+   app.use(morgan('dev'));
 } else {
-	app.use(cors({
-		origin: process.env.CLIENT_URL
-	}))
+   app.use(
+      cors({
+         origin: process.env.CLIENT_URL,
+      }),
+   );
 }
-
 
 // global error
 app.use(errorHandler);
 
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`server running on port ${port}`))
+app.listen(port, () => console.log(`server running on port ${port}`));
