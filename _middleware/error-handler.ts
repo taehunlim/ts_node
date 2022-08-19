02fb4ba993b6f682df7d2bd87630dc72a2b89004
taehunlim <dsa1520@naver.com>
2022-08-19 17:07:43 +0900

    Refactor: tsconfig.json

--- _middleware/error-handler.ts
+++ _middleware/error-handler.ts
@@ -1,38 +1,35 @@
-function errorHandler (err, req, res, next) {
-	switch(true) {
+import { Response, NextFunction } from 'express';
 
-		case typeof err === "string":
-			const is404 = err.toLowerCase().endsWith("Not Found");
-			const statusCode = is404 ? 404 : 400;
-			return (
-				res.status(statusCode).json({
-					message: err
-				})
-			);
-		
+function errorHandler(
+   err: Error,
+   _req: Request,
+   res: Response,
+   _next: NextFunction,
+) {
+   switch (true) {
+      case typeof err === 'string':
+         const is404 = err.toString().endsWith('Not Found');
+         const statusCode = is404 ? 404 : 400;
+         return res.status(statusCode).json({
+            message: err,
+         });
 
-		//mongoose error
-		case err.name === "ValidationError":
-			return (
-				res.status(400).json({
-					message: err.message
-				})
-			);
+      //mongoose error
+      case err.name === 'ValidationError':
+         return res.status(400).json({
+            message: err.message,
+         });
 
-		case err.name === "UnauthorizedErrpr":
-			return(
-				res.status(401).json({
-					message: err.message
-				})
-			);
+      case err.name === 'UnauthorizedError':
+         return res.status(401).json({
+            message: err.message,
+         });
 
-		default:
-			return (
-				res.status(500).json({
-					message: err.message
-				})
-			)
-	};
+      default:
+         return res.status(500).json({
+            message: err.message,
+         });
+   }
 }
 
 module.exports = errorHandler;
